8302bdce28bd47325dc9930d4fba576c3c44a301
janghyoseog <bestdevhyo1225@gmail.com>
2022-10-30 16:09:44 +0900

    style: 메서드 이름 변경

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -1,5 +1,6 @@
 package com.example.fastcampusmysql.domain.member.service;
 
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
 import com.example.fastcampusmysql.domain.member.entity.Member;
 import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
@@ -12,8 +13,8 @@ public class MemberWriteService {
 
     private final MemberRepository memberRepository;
 
-    public Member create(RegisterMemberCommand command) {
+    public MemberDto register(RegisterMemberCommand command) {
         Member member = command.toEntity();
-        return memberRepository.save(member);
+        return MemberDto.of(memberRepository.save(member));
     }
 }
